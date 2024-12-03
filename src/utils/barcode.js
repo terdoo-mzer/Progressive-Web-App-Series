880337c83efad57721dad0ed7461a065ece06a08
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2024-12-03 13:35:34 +0100

    add barcode object to test on mobile

---
+++ src/utils/barcode.js
@@ -0,0 +1,12 @@
+// check compatibility
+if (!("BarcodeDetector" in globalThis)) {
+    alert("Barcode Detector is not supported by this browser.");
+  } else {
+    alert("Barcode Detector supported!");
+  
+    // create new detector
+    const barcodeDetector = new BarcodeDetector({
+      formats: ["code_39", "codabar", "ean_13"],
+    });
+  }
+  
